004bb6efc6d13bd3b7c5a3185e865a9b6ecd236d
KevZ <hongqin200111@gmail.com>
2023-01-20 23:11:41 +0800

    提交到git

---
+++ basic-code/day12-code/src/StudentSystem/User.java
@@ -0,0 +1,87 @@
+package StudentSystem;
+
+public class User {
+    private String username;
+    private String password;
+    private String id;
+    private String phone;
+
+
+    public User() {
+    }
+
+    public User(String username, String password, String id, String phone) {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.phone = phone;
+    }
+
+    /**
+     * 获取
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获取
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 设置
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取
+     * @return phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * 设置
+     * @param phone
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String toString() {
+        return "User{username = " + username + ", password = " + password + ", id = " + id + ", phone = " + phone + "}";
+    }
+}
